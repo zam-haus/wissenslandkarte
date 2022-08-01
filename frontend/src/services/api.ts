a945fcf084cb951e7bf2fdb84aec6090f8825f7a
Tilman Adler <tilman-git@t-animal.de>
2022-08-01 18:23:06 +0200

    Extract loading from an endpoint into a method

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,46 +1,41 @@
 import type { ProjectDTO } from "../../../mock-backend/mocks/models/project";
 import type { UserDTO } from "../../../mock-backend/mocks/models/user";
 
-export async function loadProjects(): Promise<ProjectDTO[]> {
+const endpoints = {
+    allProjects: () => `/api/projects`,
+    allUsers: () => `api/users`
+}
+
+async function loadFromEndpoint<T>(endpoint: string): Promise<T> {
     const abortController = new AbortController();
     const signal = abortController.signal;
 
     let response: Response
-    
+
     try {
-        response = await fetch("/api/projects", {signal});
+        response = await fetch(endpoint, { signal });
     } catch (err) {
         console.log(err);
-        throw new Error('no response');
+        throw new Error(`no response loading ${endpoint}`);
     }
-    
+
     if (!response.ok) {
-        throw new Error("could no load projects");
+        throw new Error(`error loading ${endpoint}: ${response.status} ${response.statusText}`);
     }
 
-    const projects = await response.json() as ProjectDTO[];
+    return response.json();
+}
 
-    return projects.map((p) => ({...p, creationDate: new Date(p.creationDate), latestModificationDate: new Date(p.latestModificationDate)}));
+export async function loadProjects(): Promise<ProjectDTO[]> {
+    const projects = await loadFromEndpoint<ProjectDTO[]>(endpoints.allProjects());
+
+    return projects.map((p) => ({
+        ...p,
+        creationDate: new Date(p.creationDate),
+        latestModificationDate: new Date(p.latestModificationDate)
+    }));
 }
 
 export async function loadUsers(): Promise<UserDTO[]> {
-    const abortController = new AbortController();
-    const signal = abortController.signal;
-
-    let response: Response
-    
-    try {
-        response = await fetch("/api/users", {signal});
-    } catch (err) {
-        throw new Error('no response');
-    }
-    
-    if (!response.ok) {
-        throw new Error("could no load users");
-    }
-
-    const users = await response.json() as UserDTO[];
-
-    return users;
+    return loadFromEndpoint(endpoints.allUsers());
 }
-
