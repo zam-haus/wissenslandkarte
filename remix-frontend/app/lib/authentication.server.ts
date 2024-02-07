445fd8b37b04a290eef3b9c04bd966f3b5eb5dae
Tilman Adler <tilman-git@t-animal.de>
2024-02-07 22:04:43 +0100

    Enable a fake login for local development w/o keycloak

--- remix-frontend/app/lib/authentication.server.ts
+++ remix-frontend/app/lib/authentication.server.ts
@@ -1,5 +1,6 @@
 import type { User } from "@prisma/client";
 import { Authenticator } from "remix-auth";
+import { FormStrategy } from "remix-auth-form";
 import { KeycloakStrategy } from "remix-keycloak";
 import invariant from "tiny-invariant";
 
@@ -8,6 +9,7 @@ import { prisma } from "~/db.server";
 import { sessionStorage } from "./session";
 
 export const zamKeycloakStrategyName = "zam-keycloak";
+export const fakeLoginOnDevStrategyName = "fake-login";
 
 export const enum LoginErrorReason {
   emailNotVerified = "emailNotVerified",
@@ -19,58 +21,81 @@ function getFromEnvOrThrow(key: string): string {
   return value;
 }
 
-const zamKeycloak = new KeycloakStrategy(
-  {
-    useSSL: true,
-    domain: getFromEnvOrThrow("AUTH_ZAM_KEYCLOAK_DOMAIN"),
-    realm: getFromEnvOrThrow("AUTH_ZAM_KEYCLOAK_REALM"),
-    clientID: getFromEnvOrThrow("AUTH_ZAM_KEYCLOAK_CLIENT_ID"),
-    clientSecret: getFromEnvOrThrow("AUTH_ZAM_KEYCLOAK_CLIENT_SECRET"),
-    callbackURL:
-      getFromEnvOrThrow("AUTH_CALLBACK_BASE") + "/auth/zam-keycloak/callback",
-  },
-  async ({ profile }): Promise<User> => {
-    const user = await prisma.user.findFirst({
-      where: { keycloakId: profile.id },
-    });
-
-    if (user !== null) {
-      if (
-        user.firstName !== profile.name.givenName ||
-        user.lastName !== profile.name.familyName
-      ) {
-        await prisma.user.update({
-          data: {
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-          },
-          where: { id: user.id },
-        });
-      }
-      return user;
-    }
-
-    const createdUser = await prisma.user.create({
-      data: {
-        keycloakId: profile.id,
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        contactEmailAddress: profile.emails[0].value,
-        isContactEmailAddressPublic: false,
-        registrationDate: new Date(), // TODO: Is this correct or do we need some other kind of date?
-        username: profile.displayName,
-      },
-    });
-
-    if (createdUser !== null) {
-      return createdUser;
-    }
-
-    throw Error("Could not login and/or register");
-  }
-);
-
 export const authenticator = new Authenticator<User>(sessionStorage, {
   throwOnError: true,
 });
-authenticator.use(zamKeycloak, zamKeycloakStrategyName);
+
+if (process.env["DANGER_ENABLE_FAKE_LOGIN_ON_DEV"] === "true") {
+  if (process.env.NODE_ENV !== "development") {
+    console.warn(
+      "WARNING: DANGER_ENABLE_FAKE_LOGIN_ON_DEV should not be set in non-dev mode. Ignoring."
+    );
+  } else {
+    const requiredPassword = getFromEnvOrThrow("DANGER_FAKE_LOGIN_PASSWORD");
+    const fakeLoginStrategy = new FormStrategy(async ({ form }) => {
+      let password = form.get("password");
+      if (password !== requiredPassword) {
+        throw Error("Could not login and/or register");
+      }
+      const user = await prisma.user.findFirst();
+      if (user === null) {
+        throw Error("No user found.");
+      }
+      return user;
+    });
+    authenticator.use(fakeLoginStrategy, fakeLoginOnDevStrategyName);
+  }
+}
+
+if (process.env["AUTH_ENABLE_ZAM_KEYCLOACK"]) {
+  const zamKeycloak = new KeycloakStrategy(
+    {
+      useSSL: true,
+      domain: getFromEnvOrThrow("AUTH_ZAM_KEYCLOAK_DOMAIN"),
+      realm: getFromEnvOrThrow("AUTH_ZAM_KEYCLOAK_REALM"),
+      clientID: getFromEnvOrThrow("AUTH_ZAM_KEYCLOAK_CLIENT_ID"),
+      clientSecret: getFromEnvOrThrow("AUTH_ZAM_KEYCLOAK_CLIENT_SECRET"),
+      callbackURL: getFromEnvOrThrow("AUTH_CALLBACK_BASE") + "/auth/zam-keycloak/callback",
+    },
+    async ({ profile }): Promise<User> => {
+      const user = await prisma.user.findFirst({
+        where: { keycloakId: profile.id },
+      });
+
+      if (user !== null) {
+        if (
+          user.firstName !== profile.name.givenName ||
+          user.lastName !== profile.name.familyName
+        ) {
+          await prisma.user.update({
+            data: {
+              firstName: profile.name.givenName,
+              lastName: profile.name.familyName,
+            },
+            where: { id: user.id },
+          });
+        }
+        return user;
+      }
+
+      const createdUser = await prisma.user.create({
+        data: {
+          keycloakId: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          contactEmailAddress: profile.emails[0].value,
+          isContactEmailAddressPublic: false,
+          registrationDate: new Date(), // TODO: Is this correct or do we need some other kind of date?
+          username: profile.displayName,
+        },
+      });
+
+      if (createdUser !== null) {
+        return createdUser;
+      }
+
+      throw Error("Could not login and/or register");
+    }
+  );
+  authenticator.use(zamKeycloak, zamKeycloakStrategyName);
+}
--- remix-frontend/app/routes/login.tsx
+++ remix-frontend/app/routes/login.tsx
@@ -1,4 +1,5 @@
-import { Form, Outlet } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Form, Outlet, useLoaderData } from "@remix-run/react";
 import { useTranslation } from "react-i18next";
 
 import { Page } from "~/components/page/page";
@@ -7,8 +8,29 @@ export const handles = {
   i18n: ["login"],
 };
 
+export async function loader() {
+  const fakeLoginEnabled =
+    process.env["DANGER_ENABLE_FAKE_LOGIN_ON_DEV"] === "true" &&
+    process.env.NODE_ENV === "development";
+
+  return json({ fakeLoginEnabled });
+}
+
 export default function Login() {
   const { t } = useTranslation("login");
+  const { fakeLoginEnabled } = useLoaderData<typeof loader>();
+
+  if (fakeLoginEnabled) {
+    return (
+      <Page title={t("main-headline")}>
+        <Form action="/auth/fake-dev-login" method="post">
+          <input type="password" name="password" />
+          <button type="submit">Do the fake login</button>
+          <Outlet></Outlet>
+        </Form>
+      </Page>
+    );
+  }
 
   return (
     <Page title={t("main-headline")}>
