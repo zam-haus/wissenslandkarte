c5bee2e833a9a5f3e77873e45d2aa20ac9e79a04
Tilman Adler <tilman-git@t-animal.de>
2024-04-19 19:28:40 +0200

    Create a migration file for the current database version

---
+++ remix-frontend/prisma/migrations/20240418201323_init/migration.sql
@@ -0,0 +1,134 @@
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "keycloakId" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "image" TEXT,
+    "description" TEXT,
+    "registrationDate" DATETIME NOT NULL,
+    "phoneNumber" TEXT,
+    "contactEmailAddress" TEXT,
+    "isContactEmailAddressPublic" BOOLEAN NOT NULL DEFAULT false,
+    "setupCompleted" BOOLEAN NOT NULL DEFAULT false
+);
+
+-- CreateTable
+CREATE TABLE "Attachment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "type" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "creationDate" DATETIME NOT NULL,
+    "projectId" TEXT,
+    "projectUpdateId" TEXT,
+    CONSTRAINT "Attachment_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Attachment_projectUpdateId_fkey" FOREIGN KEY ("projectUpdateId") REFERENCES "ProjectUpdate" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "mainPhoto" TEXT,
+    "creationDate" DATETIME NOT NULL,
+    "latestModificationDate" DATETIME NOT NULL,
+    "needsProjectArea" BOOLEAN NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "ProjectUpdate" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "creationDate" DATETIME NOT NULL,
+    "description" TEXT NOT NULL,
+    "projectId" TEXT,
+    CONSTRAINT "ProjectUpdate_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_TagToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_TagToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Tag" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_TagToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_projectOwners" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_projectOwners_A_fkey" FOREIGN KEY ("A") REFERENCES "Project" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_projectOwners_B_fkey" FOREIGN KEY ("B") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_projectMembers" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_projectMembers_A_fkey" FOREIGN KEY ("A") REFERENCES "Project" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_projectMembers_B_fkey" FOREIGN KEY ("B") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_ProjectToTag" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_ProjectToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "Project" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_ProjectToTag_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_name_key" ON "Tag"("name");
+
+-- CreateIndex
+CREATE INDEX "Tag_name_idx" ON "Tag"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_keycloakId_key" ON "User"("keycloakId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE INDEX "User_username_keycloakId_idx" ON "User"("username", "keycloakId");
+
+-- CreateIndex
+CREATE INDEX "Attachment_projectId_projectUpdateId_idx" ON "Attachment"("projectId", "projectUpdateId");
+
+-- CreateIndex
+CREATE INDEX "Project_title_description_idx" ON "Project"("title", "description");
+
+-- CreateIndex
+CREATE INDEX "ProjectUpdate_projectId_idx" ON "ProjectUpdate"("projectId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TagToUser_AB_unique" ON "_TagToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TagToUser_B_index" ON "_TagToUser"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_projectOwners_AB_unique" ON "_projectOwners"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_projectOwners_B_index" ON "_projectOwners"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_projectMembers_AB_unique" ON "_projectMembers"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_projectMembers_B_index" ON "_projectMembers"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ProjectToTag_AB_unique" ON "_ProjectToTag"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ProjectToTag_B_index" ON "_ProjectToTag"("B");
