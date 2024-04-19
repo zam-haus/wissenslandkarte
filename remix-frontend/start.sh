f3f0c332f1a2210a6f4df550fc6a8ab417ff68bb
Tilman Adler <tilman-git@t-animal.de>
2024-04-19 19:40:41 +0200

    Ensure DB path is absolute or start.sh doesn't work

--- remix-frontend/start.sh
+++ remix-frontend/start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This file is how Fly starts the server (configured in fly.toml). Before starting
 # the server though, we need to run any prisma migrations that haven't yet been
@@ -6,5 +6,12 @@
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
 set -ex
+
+if  [[ "$DATABASE_URL" != /* ]]; then
+ echo "Database url must be an absolute path"
+ exit 1
+fi
+
+
 npx prisma migrate deploy
 npm run start
