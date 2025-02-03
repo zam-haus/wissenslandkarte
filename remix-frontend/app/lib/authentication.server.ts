82b52953dfdd8aa682189f53f0f85bced913247d
Tilman Adler <tilman-git@t-animal.de>
2025-02-03 22:31:56 +0100

    Refactor authentication strategy setup

--- remix-frontend/app/lib/authentication.server.ts
+++ remix-frontend/app/lib/authentication.server.ts
@@ -1,6 +1,9 @@
 import type { User } from "@prisma/client";
+import type { StrategyVerifyCallback } from "remix-auth";
 import { Authenticator } from "remix-auth";
 import { FormStrategy } from "remix-auth-form";
+import type { OAuth2StrategyVerifyParams } from "remix-auth-oauth2";
+import type { KeycloakExtraParams, KeycloakProfile } from "remix-keycloak";
 import { KeycloakStrategy } from "remix-keycloak";
 
 import { prisma } from "~/db.server";
@@ -19,79 +22,106 @@ export const authenticator = new Authenticator<User>(sessionStorage, {
   throwOnError: true,
 });
 
-if (environment.auth.DANGER_ENABLE_FAKE_LOGIN_ON_DEV) {
-  if (!environment.IS_DEV_MODE) {
-    console.warn(
-      "WARNING: DANGER_ENABLE_FAKE_LOGIN_ON_DEV should not be set in non-dev mode. Ignoring."
-    );
-  } else {
-    const requiredPassword = environment.auth.DANGER_FAKE_LOGIN_PASSWORD;
-    const fakeLoginStrategy = new FormStrategy(async ({ form }) => {
-      let password = form.get("password");
-      if (password !== requiredPassword) {
-        throw Error("Could not login and/or register");
-      }
-      const user = await prisma.user.findFirst();
-      console.log(user);
-      if (user === null) {
-        console.error("No user found in database. Fake login failed");
-        throw Error("No user found.");
-      }
-      return user;
-    });
-    authenticator.use(fakeLoginStrategy, fakeLoginOnDevStrategyName);
+setupFakeLoginStrategy();
+setupZamKeycloakLogin();
+
+function setupFakeLoginStrategy() {
+  if (environment.auth.DANGER_ENABLE_FAKE_LOGIN_ON_DEV) {
+    if (!environment.IS_DEV_MODE) {
+      console.warn(
+        "WARNING: DANGER_ENABLE_FAKE_LOGIN_ON_DEV should not be set in non-dev mode. Ignoring."
+      );
+    } else {
+      const requiredPassword = environment.auth.DANGER_FAKE_LOGIN_PASSWORD;
+      const fakeLoginStrategy = new FormStrategy(async ({ form }) => {
+        let password = form.get("password");
+        if (password !== requiredPassword) {
+          throw Error("Could not login and/or register");
+        }
+        const user = await prisma.user.findFirst();
+        console.log(user);
+        if (user === null) {
+          console.error("No user found in database. Fake login failed");
+          throw Error("No user found.");
+        }
+        return user;
+      });
+      authenticator.use(fakeLoginStrategy, fakeLoginOnDevStrategyName);
+    }
   }
 }
 
-if (environment.auth.ENABLE_ZAM_KEYCLOACK) {
-  const zamKeycloak = new KeycloakStrategy(
-    {
-      useSSL: true,
-      domain: environment.auth.ZAM_KEYCLOAK_DOMAIN,
-      realm: environment.auth.ZAM_KEYCLOAK_REALM,
-      clientID: environment.auth.ZAM_KEYCLOAK_CLIENT_ID,
-      clientSecret: environment.auth.ZAM_KEYCLOAK_CLIENT_SECRET,
-      callbackURL: environment.auth.CALLBACK_BASE + "/auth/zam-keycloak/callback",
-    },
-    async ({ profile }): Promise<User> => {
-      const user = await prisma.user.findFirst({
-        where: { keycloakId: profile.id },
-      });
+function setupZamKeycloakLogin() {
+  if (environment.auth.ENABLE_ZAM_KEYCLOACK) {
+    const zamKeycloak = new KeycloakStrategy(
+      {
+        useSSL: true,
+        domain: environment.auth.ZAM_KEYCLOAK_DOMAIN,
+        realm: environment.auth.ZAM_KEYCLOAK_REALM,
+        clientID: environment.auth.ZAM_KEYCLOAK_CLIENT_ID,
+        clientSecret: environment.auth.ZAM_KEYCLOAK_CLIENT_SECRET,
+        callbackURL: environment.auth.CALLBACK_BASE + "/auth/zam-keycloak/callback",
+      },
+      handleKeycloakLogin("zamKeycloak")
+    );
+    authenticator.use(zamKeycloak, zamKeycloakStrategyName);
+  }
+}
 
-      if (user !== null) {
-        if (
-          user.firstName !== profile.name.givenName ||
-          user.lastName !== profile.name.familyName
-        ) {
-          await prisma.user.update({
-            data: {
-              firstName: profile.name.givenName,
-              lastName: profile.name.familyName,
-            },
-            where: { id: user.id },
-          });
-        }
-        return user;
+function handleKeycloakLogin(
+  keycloakInstanceName: string
+): StrategyVerifyCallback<User, OAuth2StrategyVerifyParams<KeycloakProfile, KeycloakExtraParams>> {
+  return async ({ profile }) => {
+    const user = await prisma.user.findFirst({
+      where: { keycloakId: keycloakInstanceName + profile.id },
+    });
+
+    if (user !== null) {
+      if (user.firstName !== profile.name.givenName || user.lastName !== profile.name.familyName) {
+        await updateUserData(user, profile);
       }
-
-      const createdUser = await prisma.user.create({
-        data: {
-          keycloakId: profile.id,
-          firstName: profile.name.givenName,
-          lastName: profile.name.familyName,
-          contactEmailAddress: profile.emails[0].value,
-          isContactEmailAddressPublic: false,
-          registrationDate: new Date(), // TODO: Is this correct or do we need some other kind of date?
-          username: profile.displayName,
-        },
-      });
-
-      if (createdUser !== null) {
-        return createdUser;
-      }
-
-      throw Error("Could not login and/or register");
+      return user;
     }
-  );
-  authenticator.use(zamKeycloak, zamKeycloakStrategyName);
+
+    const createdUser = await createNewUser(profile, keycloakInstanceName);
+    if (createdUser !== null) {
+      return createdUser;
+    }
+
+    throw Error("Could not login and/or register");
+  };
+}
+
+async function createNewUser(profile: KeycloakProfile, idScope: string): Promise<User> {
+  const isUsernameAvailable = async (username: string) =>
+    (await prisma.user.findFirst({ where: { username } })) !== null;
+
+  let username = profile.displayName;
+  let suffix = 0;
+  while (!(await isUsernameAvailable(username))) {
+    username = profile.displayName + suffix;
+    suffix++;
+  }
+
+  return await prisma.user.create({
+    data: {
+      keycloakId: idScope + profile.id,
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      contactEmailAddress: profile.emails[0].value,
+      isContactEmailAddressPublic: false,
+      registrationDate: new Date(),
+      username,
+    },
+  });
+}
+
+async function updateUserData(user: User, profile: KeycloakProfile): Promise<void> {
+  await prisma.user.update({
+    data: {
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+    },
+    where: { id: user.id },
+  });
 }
