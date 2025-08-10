0633de9b08dfa40045f3f59149e4fbaca1fc3ca0
Tilman Adler <tilman-git@t-animal.de>
2025-08-10 17:33:25 +0200

    Authorize user before uploading in new step creation

---
+++ frontend/app/routes/projects+/$projectId.step.new.tsx
@@ -0,0 +1,135 @@
+import type { ActionFunctionArgs, LoaderFunctionArgs, TypedResponse } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { useActionData, useLoaderData } from "@remix-run/react";
+import { useTranslation } from "react-i18next";
+import { serverOnly$ } from "vite-env-only/macros";
+
+import { getProjectDetails, getProjectsByUser } from "~/database/repositories/projects.server";
+import { createProjectStep } from "~/database/repositories/projectSteps.server";
+import {
+  getLoggedInUser,
+  isAnyUserFromListLoggedIn,
+  loggedInUserHasRole,
+  Roles,
+} from "~/lib/authorization.server";
+import { descendingByDatePropertyComparator } from "~/lib/compare";
+import { assertExistsOr400 } from "~/lib/dataValidation";
+import { logger } from "~/lib/logging.server";
+import { upsertProjectStepToSearchIndex } from "~/lib/search.server";
+import { MAX_UPLOAD_SIZE_IN_BYTE } from "~/lib/upload/constants";
+import { parseMultipartFormDataUploadFilesToS3 } from "~/lib/upload/pipeline.server";
+
+import { getStringArray, getTrimmedStringsDefaultEmpty } from "../../lib/formDataParser";
+
+import { StepForm } from "./components/step-form";
+
+const FIELD_EMPTY = "FIELD_EMPTY";
+const CREATE_FAILED = "CREATE_FAILED";
+
+// Only use in server functions!
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const assertAuthorization = serverOnly$(
+  async (
+    request: Request,
+    project: { id: string; owners: { id: string }[]; members: { id: string }[] },
+  ) => {
+    const isOwnerLoggedIn = await isAnyUserFromListLoggedIn(request, project.owners);
+    const isMemberLoggedIn = await isAnyUserFromListLoggedIn(request, project.members);
+    const isProjectAdminLoggedIn = await loggedInUserHasRole(request, Roles.ProjectEditor);
+
+    if (!(isOwnerLoggedIn || isMemberLoggedIn || isProjectAdminLoggedIn)) {
+      logger("step-new").warn(
+        `Someone tried creating a new step for project ${project.id} but was not authorized to do so`,
+      );
+      throw redirect("/");
+    }
+  },
+)!;
+
+export const action = async ({
+  request,
+  params,
+}: ActionFunctionArgs): Promise<TypedResponse<never> | { error: string; exception?: string }> => {
+  assertExistsOr400(params.projectId, `Missing project id`);
+
+  const { projectId } = params;
+
+  const project = await getProjectDetails(projectId);
+  if (project === null) {
+    return {
+      error: CREATE_FAILED,
+      exception: "No such project",
+    };
+  }
+
+  await assertAuthorization(request, project);
+
+  const formData = await parseMultipartFormDataUploadFilesToS3(request, ["imageAttachments"]);
+  const { description } = getTrimmedStringsDefaultEmpty(formData, "description");
+  const { imageAttachments } = getStringArray(formData, "imageAttachments");
+
+  if (description.length === 0) {
+    return {
+      error: FIELD_EMPTY,
+    };
+  }
+  try {
+    const result = await createProjectStep({
+      description,
+      projectId,
+      imageAttachmentUrls: imageAttachments,
+    });
+
+    await upsertProjectStepToSearchIndex(result);
+
+    return redirect(`/projects/${projectId}`);
+  } catch (e: unknown) {
+    if (!(e instanceof Error)) {
+      return { error: CREATE_FAILED };
+    }
+    return {
+      error: CREATE_FAILED,
+      exception: e.message,
+    };
+  }
+};
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const user = await getLoggedInUser(request, { ifNotLoggedInRedirectTo: "/" });
+
+  const projects = await getProjectsByUser(user.username);
+
+  return { projects };
+};
+
+export default function CreateStep() {
+  const currentPath = "/projects/step/new";
+  const { projects } = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
+  const { t } = useTranslation("projects");
+
+  if (projects.length === 0) {
+    return <main>{t("no-projects")}</main>;
+  }
+
+  const projectsWithDates = projects;
+  projectsWithDates.sort(descendingByDatePropertyComparator("latestModificationDate"));
+
+  return (
+    <main>
+      {actionData?.error === FIELD_EMPTY ? <div>{t("missing-description")}</div> : null}
+      {actionData?.error === CREATE_FAILED ? (
+        <div>
+          {t("creation-failed")} {actionData.exception}
+        </div>
+      ) : null}
+
+      <StepForm
+        action={currentPath}
+        maxImageSize={MAX_UPLOAD_SIZE_IN_BYTE}
+        projectsWithDates={projectsWithDates}
+        mode="create"
+      ></StepForm>
+    </main>
+  );
+}
--- frontend/app/routes/projects+/step.new.tsx
+++ frontend/app/routes/projects+/step.new.tsx
@@ -1,135 +1,23 @@
-import type { ActionFunctionArgs, LoaderFunctionArgs, TypedResponse } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { useActionData, useLoaderData } from "@remix-run/react";
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { useTranslation } from "react-i18next";
-import { serverOnly$ } from "vite-env-only/macros";
 
-import { getProjectDetails, getProjectsByUser } from "~/database/repositories/projects.server";
-import { createProjectStep } from "~/database/repositories/projectSteps.server";
-import {
-  getLoggedInUser,
-  isAnyUserFromListLoggedIn,
-  loggedInUserHasRole,
-  Roles,
-} from "~/lib/authorization.server";
-import { descendingByDatePropertyComparator } from "~/lib/compare";
-import { logger } from "~/lib/logging.server";
-import { upsertProjectStepToSearchIndex } from "~/lib/search.server";
-import { MAX_UPLOAD_SIZE_IN_BYTE } from "~/lib/upload/constants";
-import { parseMultipartFormDataUploadFilesToS3 } from "~/lib/upload/pipeline.server";
-
-import { getStringArray, getTrimmedStringsDefaultEmpty } from "../../lib/formDataParser";
-
-import { StepForm } from "./components/step-form";
-
-const FIELD_EMPTY = "FIELD_EMPTY";
-const CREATE_FAILED = "CREATE_FAILED";
-
-// Only use in server functions!
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const assertAuthorization = serverOnly$(
-  async (
-    request: Request,
-    project: { id: string; owners: { id: string }[]; members: { id: string }[] },
-  ) => {
-    const isOwnerLoggedIn = await isAnyUserFromListLoggedIn(request, project.owners);
-    const isMemberLoggedIn = await isAnyUserFromListLoggedIn(request, project.members);
-    const isProjectAdminLoggedIn = await loggedInUserHasRole(request, Roles.ProjectEditor);
-
-    if (!(isOwnerLoggedIn || isMemberLoggedIn || isProjectAdminLoggedIn)) {
-      logger("step-new").warn(
-        `Someone tried creating a new step for project ${project.id} but was not authorized to do so`,
-      );
-      throw redirect("/");
-    }
-  },
-)!;
-
-export const action = async ({
-  request,
-}: ActionFunctionArgs): Promise<TypedResponse<never> | { error: string; exception?: string }> => {
-  const formData = await parseMultipartFormDataUploadFilesToS3(request, ["imageAttachments"]);
-
-  const { projectId, description } = getTrimmedStringsDefaultEmpty(
-    formData,
-    "projectId",
-    "description",
-  );
-  const { imageAttachments } = getStringArray(formData, "imageAttachments");
-
-  if (description.length === 0) {
-    return {
-      error: FIELD_EMPTY,
-    };
-  }
-
-  const project = await getProjectDetails(projectId);
-  if (project === null) {
-    return {
-      error: CREATE_FAILED,
-      exception: "No such project",
-    };
-  }
-
-  await assertAuthorization(request, project);
-
-  try {
-    const result = await createProjectStep({
-      description,
-      projectId,
-      imageAttachmentUrls: imageAttachments,
-    });
-
-    await upsertProjectStepToSearchIndex(result);
-
-    return redirect(`/projects/${projectId}`);
-  } catch (e: unknown) {
-    if (!(e instanceof Error)) {
-      return { error: CREATE_FAILED };
-    }
-    return {
-      error: CREATE_FAILED,
-      exception: e.message,
-    };
-  }
-};
+import { getLatestProjectId } from "~/database/repositories/projects.server";
+import { getLoggedInUser } from "~/lib/authorization.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const user = await getLoggedInUser(request, { ifNotLoggedInRedirectTo: "/" });
 
-  const projects = await getProjectsByUser(user.username);
+  const projectId = await getLatestProjectId(user.username);
 
-  return { projects };
+  if (projectId !== null) {
+    return redirect(`/projects/${projectId}/step/new`);
+  }
+
+  return {};
 };
 
 export default function CreateStep() {
-  const currentPath = "/projects/step/new";
-  const { projects } = useLoaderData<typeof loader>();
-  const actionData = useActionData<typeof action>();
   const { t } = useTranslation("projects");
 
-  if (projects.length === 0) {
-    return <main>{t("no-projects")}</main>;
-  }
-
-  const projectsWithDates = projects;
-  projectsWithDates.sort(descendingByDatePropertyComparator("latestModificationDate"));
-
-  return (
-    <main>
-      {actionData?.error === FIELD_EMPTY ? <div>{t("missing-description")}</div> : null}
-      {actionData?.error === CREATE_FAILED ? (
-        <div>
-          {t("creation-failed")} {actionData.exception}
-        </div>
-      ) : null}
-
-      <StepForm
-        action={currentPath}
-        maxImageSize={MAX_UPLOAD_SIZE_IN_BYTE}
-        projectsWithDates={projectsWithDates}
-        mode="create"
-      ></StepForm>
-    </main>
-  );
+  return <main>{t("no-projects")}</main>;
 }
