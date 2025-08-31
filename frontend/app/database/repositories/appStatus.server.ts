350469098297417da8ea85de9436e4533f3f2dad
Tilman Adler <Tilman.Adler@inovex.de>
2025-08-31 16:58:40 +0200

    Add tracking of search index rebuild to kv-store

--- frontend/app/database/repositories/appStatus.server.ts
+++ frontend/app/database/repositories/appStatus.server.ts
@@ -1,13 +1,70 @@
 import { prisma } from "~/database/db.server";
 
+import { NO_REBUILD_IN_PROGRESS } from "./appStatusEnums";
+
 const Keys = {
   searchIndexOutdated: "search-index-outdated",
+  searchIndexRebuildProgress: "search-index-rebuild-progress",
+  totalEntriesToRebuild: "total-entries-to-rebuild",
 } as const;
 
 export async function setSearchIndexOutdated(isOutdated: boolean) {
-  await prisma.keyValue.upsert({
-    where: { key: Keys.searchIndexOutdated },
-    create: { key: Keys.searchIndexOutdated, value: JSON.stringify(isOutdated) },
-    update: { value: JSON.stringify(isOutdated) },
+  await storeValue(Keys.searchIndexOutdated, isOutdated);
+}
+
+export async function isSearchIndexOutdated() {
+  return getValue(Keys.searchIndexOutdated, false, validateBoolean);
+}
+
+export async function setSearchIndexRebuildProgress(progress: number) {
+  await storeValue(Keys.searchIndexRebuildProgress, progress);
+}
+
+export async function getSearchIndexRebuildProgress() {
+  return getValue(Keys.searchIndexRebuildProgress, NO_REBUILD_IN_PROGRESS, validateNumber);
+}
+
+export async function setTotalEntriesToRebuild(total: number) {
+  await storeValue(Keys.totalEntriesToRebuild, total);
+}
+
+export async function getTotalEntriesToRebuild() {
+  return getValue(Keys.totalEntriesToRebuild, 0, validateNumber);
+}
+
+function storeValue(key: string, value: unknown) {
+  return prisma.keyValue.upsert({
+    where: { key },
+    create: { key, value: JSON.stringify(value) },
+    update: { value: JSON.stringify(value) },
   });
+}
+
+async function getValue<T>(
+  key: string,
+  defaultValue: T,
+  validator: (value: unknown) => value is T,
+) {
+  const result = await prisma.keyValue.findUnique({
+    where: { key },
+  });
+
+  if (result === null) {
+    return defaultValue;
+  }
+
+  const parsedValue = JSON.parse(result.value) as unknown;
+  if (!validator(parsedValue)) {
+    throw new Error(`Invalid value in key-value storefor key ${key}`);
+  }
+
+  return parsedValue;
+}
+
+function validateBoolean(value: unknown): value is boolean {
+  return typeof value === "boolean";
+}
+
+function validateNumber(value: unknown): value is number {
+  return typeof value === "number";
 }
---
+++ frontend/app/database/repositories/appStatusEnums.ts
@@ -0,0 +1,3 @@
+export const NO_REBUILD_IN_PROGRESS = -1;
+export const REBUILD_HAS_FAILED = -2;
+export const REBUILD_HAS_COMPLETED = -3;
